902071d6f6d5b6623aa74575855c07d7b8cd0090
JUNSEOK <kd02109@gmail.com>
2024-04-01 16:12:00 +0900

    fix: storybook main.ts 수정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,7 +8,7 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
-    '@storybook/addon-styling-webpack'
+    '@storybook/addon-styling-webpack',
   ],
   framework: {
     name: '@storybook/nextjs',
@@ -17,6 +17,6 @@ const config: StorybookConfig = {
   docs: {
     autodocs: 'tag',
   },
-  staticDirs: ['..\\public'],
+  //staticDirs: ['..\\public'],
 };
 export default config;
